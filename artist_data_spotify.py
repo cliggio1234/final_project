f51048a35bef5f062a9ffd494bf5375026e95028
Christina Liggio <cmliggio@gmail.com>
2021-04-13 20:28:01 -0400

    starting to extract data from spotify
    we have to pick what exactly we want to extract

---
+++ artist_data_spotify.py
@@ -0,0 +1,11 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time 
+
+
+client_id = "c7c042c1a50a4202b30e64d401461e2b"
+client_secret = "0585a3aaf38540d5b06743126f414cbd"
+
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
