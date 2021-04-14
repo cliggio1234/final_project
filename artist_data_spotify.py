22af58475cfd513d764ec0709b8e3881139fe3db
Christina Liggio <cmliggio@gmail.com>
2021-04-14 19:05:34 -0400

    Update artist_data_spotify.py

--- artist_data_spotify.py
+++ artist_data_spotify.py
@@ -1,15 +1,15 @@
-import spotify
-from spotify.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import time 
 
-
-client_id = "c7c042c1a50a4202b30e64d401461e2b"
-client_secret = "0585a3aaf38540d5b06743126f414cbd"
+client_id = 'c7c042c1a50a4202b30e64d401461e2b'
+client_secret = '0585a3aaf38540d5b06743126f414cbd'
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-sp = spotify.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-name = "Justin Bieber"
-result = sp.search(name)
+name = "Justin Bieber" #chosen artist
+result = sp.search(name) #search query
 result['tracks']['items'][0]['artists']
+
