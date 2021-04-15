ce02bc4a75a5a745260f88027c6eb2747615583d
addiegale <78458107+addiegale@users.noreply.github.com>
2021-04-14 20:23:09 -0400

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -5,21 +5,30 @@ import sqlite3
 import json
 import os
 
+''' Get Justin Bieber's and Taylor Swift's tweets in the last month and put it into a dictionary, gather each tweets retweets 
+    example of dictionary output {Justin Bieber: {tweet: x retweets, tweet: retweets, etc.}, Taylor Swift: {tweet: retweets, tweet: retweets, etc.}} '''
+
+
 def getLink(soup):
-    url = "https://www.zillow.com/washtenaw-county-mi/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Washtenaw%20County%2C%20MI%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.2349121040437%2C%22east%22%3A-83.15563025611971%2C%22south%22%3A42.11028501569724%2C%22north%22%3A42.565699612405055%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A2869%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%7D"
-    links = soup.findAll('div', {'class': 'list-card-price'})
-    r = requests.get(url)
+    handle = input('Input your account name on Twitter: ')
+    r = requests.get('https://twitter.com/'+ handle)
     soup = BeautifulSoup(r.text, 'html.parser')
-    homes = soup.findAll('div', {'class': 'list-card-price'})
-    forsale = []
-    for price in homes[0:10]:
-        forsale.append("https://www.zillow.com/" + price['href'])
-    return forsale 
+    all_tweets = {}
+    find_tweets = soup.find_all('li','js-stream-item')
+    if find_tweets:
+        for tweet in find_tweets:
+           soup.find('div', {'class' : 'css-1dbjc4n r-18u37iz r-1wtj0ep r-1s2bzr4 r-1mdbhws'}) 
+    else: 
+        print("Account name not found.")
+
+
+
+
 
 
 
 def main():
-    url = "https://www.zillow.com/washtenaw-county-mi/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Washtenaw%20County%2C%20MI%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.2349121040437%2C%22east%22%3A-83.15563025611971%2C%22south%22%3A42.11028501569724%2C%22north%22%3A42.565699612405055%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A2869%2C%22regionType%22%3A4%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%7D"
+    url = 'http://www.twitter.com/'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     getLink(soup)
