428d5571b3b6a600c3253a887a47ceecfee40a34
addiegale <78458107+addiegale@users.noreply.github.com>
2021-04-20 22:40:25 -0400

    Create billboardhot100.py
    Billboard hot 100 table started

---
+++ billboardhot100.py
@@ -0,0 +1,40 @@
+import os
+import sqlite3
+import unittest
+import billboard
+
+def getChart(date):
+    # starter code to pull from Billboard API from: https://rapidapi.com/LDVIN/api/billboard-api
+    data = billboard.ChartData('hot-100', date = date)
+    return data
+
+# Create Database
+def setUpDatabase(db_name):
+    """
+    * Create database to store data.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    curr = conn.cursor()
+    return curr, conn
+
+def setUpHot100Table(tablename, curr, conn):
+    """
+    * Create table containing Top 100 Billboard songs.
+    """
+    curr.execute("DROP TABLE IF EXISTS %s" % tablename)
+    curr.execute("CREATE TABLE %s (Rank INTEGER, Title TEXT, Artist TEXT)" % tablename)
+    conn.commit()
+
+def insert_data(data, tablename, curr, conn, chunksize = 25):
+    """
+    * Insert chart data in chunks of 25.
+    """
+    for index in range(0, len(data.entries), chunksize):
+        values = []
+        entries = data.entries[index:index+chunksize]
+        for rank, entry in enumerate(entries):
+            values.append('(%s,"%s","%s")' % (rank + 1 + index, entry.title, entry.artist))
+        curr.execute("INSERT INTO %s (Rank,Title,Artist) VALUES %s" % (tablename, ','.join(values)))
+        conn.commit()
+    
